adc3e6a015b0271cf8b0abd946e9edbbf30be67e
japhet john <japhetjohnk@gmail.com>
2026-02-09 20:36:45 +0000

    time

--- dist/types/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TransactionType = void 0;
-var TransactionType;
-(function (TransactionType) {
-    TransactionType["ONRAMP"] = "ONRAMP";
-    TransactionType["OFFRAMP"] = "OFFRAMP";
-})(TransactionType || (exports.TransactionType = TransactionType = {}));
-//# sourceMappingURL=index.js.map
