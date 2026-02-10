dfc17301ce659545b73f25dd5531980fccb694f7
japhet john <japhetjohnk@gmail.com>
2026-02-10 23:17:08 +0000

    sdfgh

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,8 @@ module.exports = {
         autorestart: true,
         watch: false,
         max_memory_restart: '1G',
+        exp_backoff_restart_delay: 100, // Wait 100ms then double each restart
+        restart_delay: 3000,           // Wait at least 3 seconds between restarts
         env: {
             NODE_ENV: 'production',
         }
